dacd05286922a5b72ca0bf639a08422b12955feb
russelldeguzman <russelldeguzman@ucla.edu>
2015-04-04 14:11:17 -0700

    Button Press functionality
    says you checked in when center button is pressed.
    
    TODO: communicate with android application

--- src/app_message.c
+++ src/app_message.c
@@ -1,6 +1,7 @@
 #include <pebble.h>
 
 Window *window;	
+static TextLayer *s_output_layer;;
 	
 // Key values for AppMessage Dictionary
 enum {
@@ -42,22 +43,49 @@ static void in_dropped_handler(AppMessageResult reason, void *context) {
 static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
     APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
 }
-//SINGLE DOWN CLICK
-void down_single_click_handler(ClickRecognizerRef recognizer, void *context) {
-  Window *window = (Window *)context;
+//
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect window_bounds = layer_get_bounds(window_layer);
+
+  // Create output TextLayer
+  s_output_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 5, window_bounds.size.h));
+  text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text(s_output_layer, "Want to check in?");
+  text_layer_set_overflow_mode(s_output_layer, GTextOverflowModeWordWrap);
+  layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy output TextLayer
+  text_layer_destroy(s_output_layer);
+}
+
+//SINGLE SELECT CLICK
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(s_output_layer, "Checked in!");
+
   
 }
 // register click provider 
 void config_provider(Window *window) {
- // single click / repeat-on-hold config:
-  window_single_click_subscribe(BUTTON_ID_SELECT, down_single_click_handler);
+ // single click 
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
 }
 
 
 void init(void) {
 	window = window_create();
+  
+    window_set_window_handlers(window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
 	window_stack_push(window, true);
   window_set_click_config_provider(window, (ClickConfigProvider) config_provider);
+  //window_set_click_config_provider(s_main_window, click_config_provider);
+
 
 	// Register AppMessage handlers
 	app_message_register_inbox_received(in_received_handler); 
